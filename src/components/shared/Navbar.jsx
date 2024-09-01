10233dd198d725901e4f8d62f274e0af655514dd
unknown <sojibjubayer@gmail.com>
2024-09-01 21:47:22 +0600

    recheck for css missing

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -8,19 +8,32 @@ const Navbar = () => {
                 Interview QnA
             </div>
             <ul className='flex gap-10 relative'>
-                <li><Link href="/" className='px-2 py-1'>Home</Link></li>
+                <li><Link href="/" className='px-2 py-1 hover:text-[#fe7070]'>Home</Link></li>
                 <li className='relative group'>
-                    <Link href="/" className=''>Frontend Interview QnA</Link>
-                    <ul className='absolute left-0 top-full bg-white border border-gray-200 rounded-md shadow-md mt-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10'>
-                        <li className='px-4 py-2 border-b border-gray-200'><Link href="/">HTML5</Link></li>
-                        <li className='px-4 py-2 border-b border-gray-200'><Link href="/">CSS3</Link></li>
-                        <li className='px-4 py-2 border-b border-gray-200'><Link href="/">JAVASCRIPT</Link></li>
-                        <li className='px-4 py-2 border-b border-gray-200'><Link href="/">REACT</Link></li>
-                        <li className='px-4 py-2'><Link href="/">NEXTJS</Link></li>
+                    <Link href="/" className='hover:text-[#fe7070]'>Frontend Interview QnA</Link>
+                    <ul className='absolute z-20 left-0 hidden space-y-2 bg-white border rounded-sm group-hover:block'>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/html5qna">HTML5</Link></li>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/css3qna">CSS3</Link></li>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/javascriptqna">JAVASCRIPT</Link></li>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/reactqna">REACT</Link></li>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/nextjsqna">NEXTJS</Link></li>
+                        <li className='px-4 py-2 hover:text-[#FF7777]'><Link href="/">REDUX</Link></li>
                     </ul>
                 </li>
-                <li><Link href="/" className='px-2 py-1'>Backend Interview QnA</Link></li>
-                <li><Link href="/" className='px-2 py-1'>Others</Link></li>
+                <li className='relative group'>
+                    <Link href="/" className='hover:text-[#fe7070]'>Backend Interview QnA</Link>
+                    <ul className='absolute z-20 left-0 hidden space-y-2 bg-white border rounded-sm group-hover:block'>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/nodejsqna">NODEJS</Link></li>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/expressjsqna">EXPRESSJS</Link></li>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/mongodbqna">MONGODB</Link></li>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/mongooseqna">MONGOOSE</Link></li>
+                        <li className='px-4 py-2 border-b border-gray-200 hover:text-[#FF7777]'><Link href="/jwtqna">JWT</Link></li>
+                        
+                    </ul>
+                </li>
+             
+                <li><Link href="/" className='px-2 py-1 hover:text-[#fe7070]'>Others</Link></li>
+                <li><Link href="/admin" className='px-2 py-1 hover:text-[#fe7070]'>Admin</Link></li>
             </ul>
             <div className='bg-[#FF7777] flex items-center h-full px-2'>
                 Login
