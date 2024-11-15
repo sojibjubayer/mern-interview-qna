a5655492bb97d6b04c5b7babc889d4b0e37edbc1
unknown <sojibjubayer@gmail.com>
2024-11-16 01:54:01 +0600

    localhost error

--- src/lib/getQnA.js
+++ src/lib/getQnA.js
@@ -3,7 +3,7 @@ export const dynamic="force-dynamic";
 export const getQnA = async () => {
   try {
     const res = await axios.get(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/get-all-qna`
+      `/api/get-all-qna`
     );
     
     return res.data;
