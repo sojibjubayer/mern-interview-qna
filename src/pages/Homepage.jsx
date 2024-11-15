69271090f4a852fdb3e8507ed915989ee101906d
unknown <sojibjubayer@gmail.com>
2024-11-16 02:21:34 +0600

    responsive check

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,13 +6,13 @@ const Homepage = () => {
     <div className=" space-y-5 mt-5 w-[95%] mx-auto min-h-screen">
       <div className="flex flex-col md:flex-row gap-5">
         <Link
-          className="bg-white rounded-sm   shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full  font-semibold"
+          className="bg-white rounded-sm   shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full  font-semibold"
           href="/html5qna"
         >
           <span className="text-[#FF7777]">HTML5</span>&nbsp; Interview Question & Answers
         </Link>
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/css3qna"
         >
           <span className="text-[#FF7777]">CSS3</span>&nbsp; Interview Question & Answers
@@ -20,13 +20,13 @@ const Homepage = () => {
       </div>
       <div className="flex flex-col md:flex-row gap-5">
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/javascriptqna"
         >
           <span className="text-[#FF7777]">JAVASCRIPT</span>&nbsp; Interview Question & Answers
         </Link>
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/reactqna"
         >
           <span className="text-[#FF7777]">REACT</span>&nbsp;Interview Question & Answers
@@ -34,13 +34,13 @@ const Homepage = () => {
       </div>
       <div className="flex flex-col md:flex-row gap-5">
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/reduxqna"
         >
           <span className="text-[#FF7777]">REDUX</span>&nbsp; Interview Question & Answers
         </Link>
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/nextjsqna"
         >
           <span className="text-[#FF7777]">NEXTJS</span>&nbsp; Interview Question & Answers
@@ -48,13 +48,13 @@ const Homepage = () => {
       </div>
       <div className="flex flex-col md:flex-row gap-5">
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/nodejsqna"
         >
           <span className="text-[#FF7777]">NODEJS</span>&nbsp; Interview Question & Answers
         </Link>
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/expressjsqna"
         >
           <span className="text-[#FF7777]">EXPRESSJS</span>&nbsp; Interview Question & Answers
@@ -62,13 +62,13 @@ const Homepage = () => {
       </div>
       <div className="flex flex-col md:flex-row gap-5">
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/mongodbqna"
         >
           <span className="text-[#FF7777]">MONGODB</span>&nbsp; Interview Question & Answers
         </Link>
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/mongooseqna"
         >
           <span className="text-[#FF7777]">MONGOOSE</span>&nbsp; Interview Question & Answers
@@ -76,13 +76,13 @@ const Homepage = () => {
       </div>
       <div className="flex flex-col md:flex-row gap-5">
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/firebaseqna"
         >
           <span className="text-[#FF7777]">FIREBASE</span>&nbsp; Interview Question & Answers
         </Link>
         <Link
-          className="bg-white rounded-sm  shadow-md md:h-20 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
+          className="bg-white rounded-sm  shadow-md md:h-20 lg:h-24 h-14 text-sm md:text-base flex items-center justify-center w-full font-semibold"
           href="/jwtqna"
         >
           <span className="text-[#FF7777]">JWT</span>&nbsp; Interview Question & Answers
