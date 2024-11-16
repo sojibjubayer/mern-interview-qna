1a3c2a88a36f7e92df7919c03741982bc30ffbb7
unknown <sojibjubayer@gmail.com>
2024-11-16 14:02:50 +0600

    added code for text to tag

--- src/app/(frontend)/javascriptqna/page.js
+++ src/app/(frontend)/javascriptqna/page.js
@@ -2,7 +2,8 @@
 import useQnA from "@/hooks/useQna";
 import { TbHandClick } from "react-icons/tb";
 import React, { useState } from "react";
-// export const dynamic = "force-dynamic";
+import DOMPurify from "dompurify"; // For sanitizing HTML
+
 const JAVASCRIPT = () => {
   const { allQnA, loading } = useQnA();
   const [expandedQuestionId, setExpandedQuestionId] = useState(null);
@@ -12,18 +13,21 @@ const JAVASCRIPT = () => {
   };
 
   return (
-    <div className=" flex flex-col gap-4 p-2 ">
+    <div className="flex flex-col gap-4 p-2">
       <div className="bg-white p-2 text-center md:w-[30%] mx-auto font-semibold flex flex-col items-center justify-center rounded-lg">
         <img
           src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
-          alt="JAVACRIPT Badge"
-           className="rounded-md w-32 h-10 md:w-36 md:h-11"
+          alt="JAVASCRIPT Badge"
+          className="rounded-md w-32 h-10 md:w-36 md:h-11"
         />
-        <p className="flex items-center gap-2 text-sm md:text-base"><TbHandClick className="hidden md:flex"/>click on the question to see answer</p>
+        <p className="flex items-center gap-2 text-sm md:text-base">
+          <TbHandClick className="hidden md:flex" />
+          click on the question to see the answer
+        </p>
       </div>
       {loading ? (
         <p className="flex justify-center items-center text-[#ff7777]">
-        <span className="loading loading-ball loading-lg min-h-screen"></span>
+          <span className="loading loading-ball loading-lg min-h-screen"></span>
         </p>
       ) : (
         allQnA
@@ -38,14 +42,23 @@ const JAVASCRIPT = () => {
                   {index + 1}. {qna.question}
                 </p>
                 {expandedQuestionId === qna._id && (
-                  <div className="mt-2 text-sm md:text-base">{qna.answer} {qna.code &&(<p className="bg-gray-200 p-1 rounded-md text-blue-600">Example code: {qna.code}</p>)}</div>
-                 
+                  <div className="mt-2 text-sm md:text-base">
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: DOMPurify.sanitize(qna.answer),
+                      }}
+                    />
+                    {qna.code && (
+                      <p className="bg-gray-200 p-1 rounded-md text-blue-600">
+                        Example code: {qna.code}
+                      </p>
+                    )}
+                  </div>
                 )}
               </div>
             </div>
           ))
       )}
-      
     </div>
   );
 };
